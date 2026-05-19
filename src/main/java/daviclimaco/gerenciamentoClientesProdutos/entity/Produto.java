77bca3ca17d4e6260ab9627c7f47384256436c3b
davi climaco <davibomfimclimaco@gmail.com>
2026-05-19 20:29:15 -0300

    ajustaod nome entiy

--- src/main/java/daviclimaco/gerenciamentoClientesProdutos/entity/Produto.java
+++ src/main/java/daviclimaco/gerenciamentoClientesProdutos/entity/Produto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
 
-@Entity
+@Entity(name = "Produtos")
 @Table(name = "tb_produtos")
 public class Produto {
 
